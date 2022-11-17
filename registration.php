fe57f52a238eb6e329225a503fa5f5fe23e66ff8
Bart <74151987+El-Barto3@users.noreply.github.com>
2022-11-17 19:56:23 +0100

    making login and registration work

--- registration.php
+++ registration.php
@@ -1,134 +1,133 @@
 <?php
+
 	session_start();
-		//Udana walidacja? Załóżmy, że tak!
-		$wszystko_OK=true;
+	//Udana walidacja? Załóżmy, że tak!
+	$wszystko_OK=true;
+	
+	//Sprawdź poprawność imienia i nazwiska
+	$name = $_POST['name'];
+	$surname = $_POST['surname'];
+	
+	if( $name == "")
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_name'] = "Niepoprawne imię";
+	}
 		
-		//Sprawdź poprawność imienia i nazwiska
-		$name = $_POST['name'];
-		$surname = $_POST['surname'];
+	if($surname == "")
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_surname'] = "Niepoprawne nazwisko";
+	}
+	
+	// Sprawdź poprawność adresu email
+	$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+	
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) == false)
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_email']="Niepoprawny email, wzór: adres@mail.com";
+	}
+	
+	//Sprawdź poprawność hasła
+	$haslo1 = $_POST['password1'];
+	$haslo2 = $_POST['password2'];
+	
+	if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_haslo']="Hasło musi posiadać od 8 do 20 znaków!";
+	}
+	
+	if ($haslo1!=$haslo2)
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_haslo']="Podane hasła nie są identyczne!";
+	}
 
-		if( $name == "")
+	$haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
+	
+	//Czy zaakceptowano regulamin?
+	if (!isset($_POST['regulamin']))
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
+	}				
+	
+	//ReCaptcha sprawdza poprawność
+	$sekret = "6LcaWBUjAAAAAMEirFEq1xyBO7-Krjl0244i_xuz";
+	
+	$sprawdz = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$sekret.'&response='.$_POST['g-recaptcha-response']);
+	
+	$odpowiedz = json_decode($sprawdz);
+	
+	if ($odpowiedz->success==false)
+	{
+		$wszystko_OK=false;
+		$_SESSION['e_bot']="Potwierdź, że nie jesteś robotem!";
+	}		
+	
+	//Zapamiętaj wprowadzone dane
+	$_SESSION['fr_name'] = $name;
+	$_SESSION['fr_surname'] = $surname;
+	$_SESSION['fr_email'] = $email;
+	if (isset($_POST['regulamin'])) 
+		$_SESSION['fr_regulamin'] = true;
+	
+	require 'databaselogin.php';
+	
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	
+	try 
+	{
+		$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+		$polaczenie -> query ('SET NAMES utf8');
+		$polaczenie -> query ('SET CHARACTER_SET utf8_unicode_ci');
+		if ($polaczenie->connect_errno!=0) 
 		{
-			$wszystko_OK=false;
-			$_SESSION['e_name'] = "Niepoprawne imię";
+			throw new Exception(mysqli_connect_errno());
 		}
+		else
+		{
+			//Czy email już istnieje?
+			$rezultat = $polaczenie->query("SELECT id FROM users WHERE email='$email'");
+			if (!$rezultat) throw new Exception($polaczenie->error);
 			
-		if($surname == "")
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_surname'] = "Niepoprawne nazwisko";
-		}
+			$ile_takich_maili = $rezultat->num_rows;
+			if($ile_takich_maili>0)
+			{
+				$wszystko_OK=false;
+				$_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail!";
+			}		
 
-		// Sprawdź poprawność adresu email
-		$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-		
-		if (filter_var($email, FILTER_VALIDATE_EMAIL) == false)
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_email']="Niepoprawny email, wzór: adres@mail.com";
-		}
-		
-		//Sprawdź poprawność hasła
-		$haslo1 = $_POST['password1'];
-		$haslo2 = $_POST['password2'];
-		
-		if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_haslo']="Hasło musi posiadać od 8 do 20 znaków!";
-		}
-		
-		if ($haslo1!=$haslo2)
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_haslo']="Podane hasła nie są identyczne!";
-		}	
-
-		$haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
-		
-		//Czy zaakceptowano regulamin?
-		if (!isset($_POST['regulamin']))
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
-		}				
-		
-		//ReCaptcha sprawdza poprawność
-		$sekret = "6LcaWBUjAAAAAMEirFEq1xyBO7-Krjl0244i_xuz";
-		
-		$sprawdz = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$sekret.'&response='.$_POST['g-recaptcha-response']);
-		
-		$odpowiedz = json_decode($sprawdz);
-		
-		if ($odpowiedz->success==false)
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_bot']="Potwierdź, że nie jesteś robotem!";
-		}		
-		
-		//Zapamiętaj wprowadzone dane
-		$_SESSION['fr_name'] = $name;
-		$_SESSION['fr_surname'] = $surname;
-		$_SESSION['fr_email'] = $email;
-		if (isset($_POST['regulamin'])) $_SESSION['fr_regulamin'] = true;
-		
-		$host = "rlpmyaxbaza.mysql.db";
-		$db_user = "rlpmyaxbaza";
-		$db_password = "AGDg7ZQdtvCZbgMB";
-		$db_name = "rlpmyaxbaza";	
 			
-		mysqli_report(MYSQLI_REPORT_STRICT);
-		
-		try 
-		{
-			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-			$polaczenie -> query ('SET NAMES utf8');
-			$polaczenie -> query ('SET CHARACTER_SET utf8_unicode_ci');
-			if ($polaczenie->connect_errno!=0) 
+			if ($wszystko_OK==true)
 			{
-				throw new Exception(mysqli_connect_errno());
-			}
-			else
-			{
-				//Czy email już istnieje?
-				$rezultat = $polaczenie->query("SELECT id FROM users WHERE email='$email'");
-				
-				if (!$rezultat) throw new Exception($polaczenie->error);
-				
-				$ile_takich_maili = $rezultat->num_rows;
-				if($ile_takich_maili>0)
+				//Hurra, wszystkie testy zaliczone, dodajemy gracza do bazy
+				if ($polaczenie->query("INSERT INTO users VALUES (NULL, '$name', '$surname', '$email', '$haslo_hash', CURRENT_TIMESTAMP, NULL, NULL)"))
 				{
-					$wszystko_OK=false;
-					$_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail!";
-				}		
-
-				
-				if ($wszystko_OK==true)
-				{
-					//Hurra, wszystkie testy zaliczone, dodajemy gracza do bazy
-					if ($polaczenie->query("INSERT INTO users VALUES (NULL, '$name', '$surname', '$email', '$haslo_hash', CURRENT_TIMESTAMP, NULL, NULL)"))
-					{
-						$_SESSION['udanarejestracja']=true;
-						unset($_SESSION['submit_button_register']);
-						//header('Location: ?a=news');
-						
-					}
-					else
-					{
-						throw new Exception($polaczenie->error);
-					}
+					$_SESSION['udanarejestracja']=true;
+					unset($_SESSION['submit_button_register']);
+					//header('Location: ?a=news');
 					
 				}
+				else
+				{
+					throw new Exception($polaczenie->error);
+				}
 				
-				$polaczenie->close();
 			}
 			
-		}
-		catch(Exception $e)
-		{
-			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
-			if(strpos($e, "mysqli") !== false) echo '<br />Informacja developerska: Błąd połączenia.';
-			else echo '<br />Informacja developerska: '.$e;
+			$polaczenie->close();
 		}
 		
+	}
+	catch(Exception $e)
+	{
+		echo $e;
+		echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
+		if(strpos($e, "mysqli") !== false) echo '<br />Informacja developerska: Błąd połączenia.';
+		else echo '<br />Informacja developerska: '.$e;
+	}
+
 ?> 
